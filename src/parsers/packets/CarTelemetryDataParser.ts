a6568ca4a8d49ae5f5eb09a809325b4a95568190
PrimeGoose <vim.nightmare@gmail.com>
2021-07-31 01:07:57 +0000

    linting

--- src/parsers/packets/CarTelemetryDataParser.ts
+++ src/parsers/packets/CarTelemetryDataParser.ts
@@ -14,25 +14,25 @@ export class CarTelemetryDataParser extends F1Parser {
       .uint8('m_drs')
       .uint8('m_revLightsPercent')
       .uint16le('m_revLightsBitValue')
-      .array('m_brakesTemperature',{
+      .array('m_brakesTemperature', {
         length: 4,
         type: new Parser().uint16le(''),
       })
-      .array('m_tyresSurfaceTemperature',{
+      .array('m_tyresSurfaceTemperature', {
         length: 4,
         type: new Parser().uint8(''),
       })
-      .array('m_tyresInnerTemperature',{
+      .array('m_tyresInnerTemperature', {
         length: 4,
         type: new Parser().uint8(''),
       })
       .uint16le('m_engineTemperature')
 
-      .array('m_tyresPressure',{
+      .array('m_tyresPressure', {
         length: 4,
         type: new Parser().floatle(''),
       })
-      .array('m_surfaceType',{
+      .array('m_surfaceType', {
         length: 4,
         type: new Parser().uint8(''),
       });
