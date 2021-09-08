65d47740025a997989d433450271dbf432413af1
PrimeGoose <vim.nightmare@gmail.com>
2021-09-08 02:22:19 +0100

    m_bestLapTimeInMS fix

--- src/parsers/packets/FinalClassificationDataParser.ts
+++ src/parsers/packets/FinalClassificationDataParser.ts
@@ -5,22 +5,26 @@ export class FinalClassificationDataParser extends F1Parser {
   constructor() {
     super();
 
-    this.uint8('m_position')
+    this
+      .uint8('m_position')
       .uint8('m_numLaps')
       .uint8('m_gridPosition')
       .uint8('m_points')
       .uint8('m_numPitStops')
       .uint8('m_resultStatus')
-      .uint32('m_bestLapTimeInMS')
+
+      .uint32le('m_bestLapTimeInMS')
       .doublele('m_totalRaceTime')
+
       .uint8('m_penaltiesTime')
       .uint8('m_numPenalties')
       .uint8('m_numTyreStints')
-      .array('m_tyreStintsActual', {
+      
+      .array('m_tyreStintsActual',{
         length: 8,
         type: new Parser().uint8(''),
       })
-      .array('m_tyreStintsVisual', {
+      .array('m_tyreStintsVisual',{
         length: 8,
         type: new Parser().uint8(''),
       });
