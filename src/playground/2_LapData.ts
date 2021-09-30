c262c987d3f12f9e3a9324025cfab14e3ff58101
PrimeGoose <vim.nightmare@gmail.com>
2021-09-30 02:20:04 +0100

    example output file

--- src/playground/2_LapData.ts
+++ src/playground/2_LapData.ts
@@ -1,12 +1,22 @@
-// import * as dgram from 'dgram';
 import {F1TelemetryClient, constants} from '..';
+
 const {PACKETS} = constants;
 
 const client = new F1TelemetryClient({port: 20777});
 
-client.on(PACKETS.lapData, x => {
+client.on(PACKETS.lapData, lapDataPackage => {
   console.clear();
-  console.log(x);
+
+  const data: string = JSON.stringify(lapDataPackage, (key, value) => {
+    if (typeof value === 'bigint') {
+      return value.toString() + 'n';
+    } else {
+      return value;
+    }
+  });
+
+  console.log('--------------');
+  console.log(JSON.parse(data));
 });
 
 client.start();
--- src/playground/3_Event.ts
+++ src/playground/3_Event.ts
@@ -1,7 +1,6 @@
 import {F1TelemetryClient, constants} from '..';
 const {PACKETS} = constants;
 const fsx = require('fs-extra');
-const fs = require('fs');
 
 const client = new F1TelemetryClient({port: 20777});
 
@@ -9,247 +8,31 @@ fsx.ensureDir('example-output', (error: String) => {
   console.log(error);
 });
 
-fsx.ensureFile('example-outputs/events.txt', (error: string) => {
+fsx.ensureFile('example-outputs/events.json', (error: string) => {
   console.log(error);
 });
 
+const eventArray: Array<{}> = [];
 client.on(PACKETS.event, eventPackage => {
-  let data = '';
   console.clear();
-  console.log(eventPackage);
 
-  const j = JSON.stringify(eventPackage, (key, value) => {
+  const data = JSON.stringify(eventPackage, (key, value) => {
     if (typeof value === 'bigint') {
       return value.toString() + 'n';
     } else {
       return value;
     }
   });
-  console.log(j);
+  console.log(data);
 
   console.log('--------------');
-  console.log(JSON.parse(j));
+  console.log(JSON.parse(data));
 
-  data = data + '---------------frame start---------------' + '\n';
+  eventArray.push(JSON.parse(data));
 
-  if (eventPackage?.m_header?.m_sessionUID) {
-    data =
-      data +
-      'sessionUID: ' +
-      eventPackage?.m_header?.m_sessionUID.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.m_header.m_sessionTime) {
-    data =
-      data +
-      'sessionTime: ' +
-      eventPackage?.m_header.m_sessionTime.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.m_header.m_frameIdentifier) {
-    data =
-      data +
-      'frameIdentifier: ' +
-      eventPackage?.m_header.m_frameIdentifier.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.m_eventStringCode) {
-    data =
-      data +
-      'eventStringCode: ' +
-      eventPackage?.m_eventStringCode.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.FastestLap?.vehicleIdx) {
-    data =
-      data +
-      'FastestLap vehicleIdx: ' +
-      eventPackage?.FastestLap?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.FastestLap?.lapTime) {
-    data =
-      data +
-      'FastestLap lapTime: ' +
-      eventPackage?.FastestLap?.lapTime.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Retirement?.vehicleIdx) {
-    data =
-      data +
-      'Retirement vehicleIdx: ' +
-      eventPackage?.Retirement?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.TeamMateInPits?.vehicleIdx) {
-    data =
-      data +
-      'TeamMateInPits vehicleIdx: ' +
-      eventPackage?.TeamMateInPits?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.RaceWinner?.vehicleIdx) {
-    data =
-      data +
-      'RaceWinner vehicleIdx: ' +
-      eventPackage?.RaceWinner?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Penalty?.penaltyType) {
-    data =
-      data +
-      'Penalty penaltyType: ' +
-      eventPackage?.Penalty?.penaltyType.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Penalty?.infringementType) {
-    data =
-      data +
-      'Penalty infringementType: ' +
-      eventPackage?.Penalty?.infringementType.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Penalty?.vehicleIdx) {
-    data =
-      data +
-      'Penalty vehicleIdx: ' +
-      eventPackage?.Penalty?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Penalty?.otherVehicleIdx) {
-    data =
-      data +
-      'Penalty otherVehicleIdx: ' +
-      eventPackage?.Penalty?.otherVehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Penalty?.time) {
-    data =
-      data + 'Penalty time: ' + eventPackage?.Penalty?.time.toString() + '\n';
-  }
-
-  if (eventPackage?.Penalty?.lapNum) {
-    data =
-      data +
-      'Penalty lapNum: ' +
-      eventPackage?.Penalty?.lapNum.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Penalty?.placesGained) {
-    data =
-      data +
-      'Penalty placesGained: ' +
-      eventPackage?.Penalty?.placesGained.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.SpeedTrap?.vehicleIdx) {
-    data =
-      data +
-      'SpeedTrap vehicleIdx: ' +
-      eventPackage?.SpeedTrap?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.SpeedTrap?.speed) {
-    data =
-      data +
-      'SpeedTrap speed: ' +
-      eventPackage?.SpeedTrap?.speed.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.SpeedTrap?.overallFastestInSession) {
-    data =
-      data +
-      'SpeedTrap overallFastestInSession: ' +
-      eventPackage?.SpeedTrap?.overallFastestInSession.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.SpeedTrap?.driverFastestInSession) {
-    data =
-      data +
-      'SpeedTrap driverFastestInSession: ' +
-      eventPackage?.SpeedTrap?.driverFastestInSession.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.StartLights?.numLights) {
-    data =
-      data +
-      'StartLights numLights: ' +
-      eventPackage?.StartLights?.numLights.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.StartLightsOut) {
-    data =
-      data +
-      'StartLightsOut StartLightsOut: ' +
-      eventPackage?.StartLightsOut.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.DriveThroughPenaltyServed) {
-    data =
-      data +
-      'DriveThroughPenaltyServed vehicleIdx: ' +
-      eventPackage?.DriveThroughPenaltyServed?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.StopGoPenaltyServed) {
-    data =
-      data +
-      'StopGoPenaltyServed vehicleIdx: ' +
-      eventPackage?.StopGoPenaltyServed?.vehicleIdx.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Flashback?.flashbackFrameIdentifier) {
-    data =
-      data +
-      'Flashback flashbackFrameIdentifier: ' +
-      eventPackage?.Flashback?.flashbackFrameIdentifier.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Flashback?.flashbackSessionTime) {
-    data =
-      data +
-      'Flashback flashbackSessionTime: ' +
-      eventPackage?.Flashback?.flashbackSessionTime.toString() +
-      '\n';
-  }
-
-  if (eventPackage?.Buttons?.m_buttonStatus) {
-    data =
-      data +
-      'buttonStatus: ' +
-      eventPackage?.Buttons?.m_buttonStatus.toString() +
-      '\n';
-  }
-
-  data = data + '---------------frame end---------------' + '\n';
-
-  // fs.appendFile('example-outputs/events.txt', data, (error: string) => {
-  //   console.log(error);
-  // });
+  fsx.writeJson('example-outputs/events.json', eventArray, (error: string) => {
+    console.log(error);
+  });
 });
 
 client.start();
