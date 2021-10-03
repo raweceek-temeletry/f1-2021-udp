d17c57c462c5bad642ee59d90ec4c5b89f7919e1
PrimeGoose <vim.nightmare@gmail.com>
2021-10-03 07:21:50 +0100

    updated enevts playground to include binaryButtons

--- src/playground/3_Event.ts
+++ src/playground/3_Event.ts
@@ -1,19 +1,23 @@
 import {F1TelemetryClient, constants} from '..';
+import {PacketEventData} from '../parsers/packets/types';
 const {PACKETS} = constants;
 const fsx = require('fs-extra');
 
-const client = new F1TelemetryClient({port: 20777});
+const client: F1TelemetryClient = new F1TelemetryClient({
+  port: 20777,
+  binaryButtons: true,
+});
 
-fsx.ensureDir('example-output', (error: String) => {
+fsx.ensureDir('example-output', (error: String): void => {
   console.log(error);
 });
 
-fsx.ensureFile('example-outputs/events.json', (error: string) => {
+fsx.ensureFile('example-outputs/events.json', (error: string): void => {
   console.log(error);
 });
 
 const eventArray: Array<{}> = [];
-client.on(PACKETS.event, eventPackage => {
+client.on(PACKETS.event, (eventPackage: PacketEventData): void => {
   console.clear();
 
   const data = JSON.stringify(eventPackage, (key, value) => {
@@ -27,12 +31,15 @@ client.on(PACKETS.event, eventPackage => {
 
   console.log('--------------');
   console.log(JSON.parse(data));
-
   eventArray.push(JSON.parse(data));
 
-  fsx.writeJson('example-outputs/events.json', eventArray, (error: string) => {
-    console.log(error);
-  });
+  fsx.writeJson(
+    'example-outputs/events.json',
+    eventArray,
+    (error: string): void => {
+      error ? console.log(error) : null;
+    }
+  );
 });
 
 client.start();
