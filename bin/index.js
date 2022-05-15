0f1d17efe2c9afa3aa2148b74872e64b8f2830d6
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2022-05-15 15:25:00 +0100

    log args

--- bin/index.js
+++ bin/index.js
@@ -32,14 +32,44 @@ const parser = require('f1-2021-udp').F1TelemetryClient
 const socket = dgram.createSocket('udp4');
 const args = process.argv.slice(2);
 const args2 = process.argv.slice(3);
-
+const ValidLogArgs = []
+// validate array of args to be values from 0 to 11
+function validateArgs(args) {
+    for (let i = 0; i < args.length; i++) {
+        if (args[ i ] >= 0 && args[ i ] <= 11) {
+            ValidLogArgs.push(args[ i ]);
+        }
+    }
+}
+const setOfLogArgs = new Set(ValidLogArgs);
 
 if (args.length == 0) {
     console.error(`
-    forwarding: 
-    Please enter the IP address and port number
-    Usage: Example: npx f1-2021-udp [<IP>:<PORT>]
-    Example: npx f1-2021-udp -f 192.168.1.114:20777`)
+    forwarding flags: -f or --forward
+    forwarding args: ip:port
+    Example: npx f1-2021-udp -f 192.168.1.114:20777
+
+    log flags: --log
+    log args: package id
+    Example: npx f1-2021-udp --log 1 2 3 4 5 6 7 8 9 10 11
+    0: motion
+    1: session
+    2: lap data
+    3: event
+    4: participants
+    5: car setups
+    6: car telemetry
+    7: car status
+    8: final classification
+    9: lobby info
+    10: car damage
+    11: session history
+
+    Please enter the IP address and port number or use the --log falg.
+    Forward Usage: Example: npx f1-2021-udp [<IP>:<PORT>]
+    Log Usage: Example: npx f1-2021-udp --log [<PACKAGE ID>]
+
+    `)
     process.exit(1); //an error occurred
 
 } else if (args[ 0 ] == '-f' || args[ 0 ] == '--forward') {
@@ -51,7 +81,7 @@ if (args.length == 0) {
 
             validArgs.push({ ip: ip, port: port });
             forward()
-            
+
         } else {
             console.error(`
             "${arg}"'is not valid!
@@ -66,70 +96,92 @@ if (args.length == 0) {
         };
     });
 } else if (args[ 0 ] == '--log' || args[ 0 ] == '-l') {
-    
+
     const client = new parser();
-    
+
     // motion 0
-    client.on('motion',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(0)) {
+        client.on('motion', function (data) {
+            console.log(data);
+        })
+    }
     // session 1
-    client.on('session',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(1)) {
+        client.on('session', function (data) {
+            console.log(data);
+        })
+    }
     // lap data 2
-    client.on('lapData',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(2)) {
+        client.on('lapData', function (data) {
+            console.log(data);
+        })
+    }
+
     // event 3
-    client.on('event',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(3)) {
+        client.on('event', function (data) {
+            console.log(data);
+        })
+    }
+
     // participants 4
-    client.on('participants',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(4)) {
+        client.on('participants', function (data) {
+            console.log(data);
+        })
+    }
+
     // car setup 5
-    client.on('carSetups',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(5)) {
+        client.on('carSetups', function (data) {
+            console.log(data);
+        })
+    }
+
     // car telemetry 6
-    client.on('carTelemetry',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(6)) {
+        client.on('carTelemetry', function (data) {
+            console.log(data);
+        })
+    }
+
     // car status 7
-    client.on('carStatus',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(7)) {
+        client.on('carStatus', function (data) {
+            console.log(data);
+        })
+    }
+
     // final classification 8
-    client.on('finalClassification',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(8)) {
+        client.on('finalClassification', function (data) {
+            console.log(data);
+        })
+    }
+
     // lobby info 9
-    client.on('lobbyInfo',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(9)) {
+        client.on('lobbyInfo', function (data) {
+            console.log(data);
+        })
+    }
+
     // car damage 10
-    client.on('carDamage',function(data) {
-        console.log(data);
-    })
-    
+    if (setOfLogArgs.has(10)) {
+        client.on('carDamage', function (data) {
+            console.log(data);
+        })
+    }
+
     // session history 11
-    client.on('sessionHistory',function(data) {
-        console.log(data);
-    })
-    
-    
+    if (setOfLogArgs.has(11)) {
+        client.on('sessionHistory', function (data) {
+            console.log(data);
+        })
+    }
+
+
     // to start listening:
     client.start();
 }
